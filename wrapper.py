f439b33302a46976541928c7dd61d29da4680d2e
cagin24 <47904355+cagin24@users.noreply.github.com>
2022-08-03 15:03:29 +0300

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,13 +1,12 @@
 import ctypes
  
- 
-#loading dll into memory
- 
-DLLFile = ctypes.CDLL("C:/Users/kemal.sertkaya/Desktop/wrapper/Dll1.dll") #specify the path of the DLL file
-#Simple function call with no return type
+#loading dll file 
+
+DLLFile = ctypes.CDLL("path/to/dll/file") 
+#Simple function call with no return type, -first function
 DLLFile.foo()
  
-#Passing integer type arguments and returning sum
+#Second function, Passing integer type arguments and returning sum
 print("\nSum:" , (DLLFile.MyAdd(4,5)))
  
 print(DLLFile)
@@ -16,37 +15,9 @@ print(DLLFile)
 DLLFile.textreturn.argtypes = [ctypes.c_char_p,ctypes.c_char_p,ctypes.c_size_t] #defining the datatype of arguments
 DLLFile.textreturn.restype = ctypes.c_char_p #defining return type
  
-phrase = b"Hellloooooo" #b' used to specify its a byte string
+phrase = b"Hello"
 result = ctypes.create_string_buffer(100)
 res = DLLFile.textreturn(phrase,result,ctypes.sizeof(result))
 print(res)
 print(result.value)
  
-"""
-functionProtoptype = ctypes.WINFUNCTYPE(
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_bool,
-    ctypes.c_bool,
-    ctypes.c_char,
-    ctypes.c_char,
-    ctypes.c_char
-)
-functionparameters = (1050,0,0,True,True,"", 0, 0)
- 
-functionAPI = functionProtoptype(("ShowNumber",DLLFile2),functionparameters)
- 
-p1 = ctypes.c_int (1050)
-p2 = ctypes.c_int (0)
-p3 = ctypes.c_int (0)
-p4 = ctypes.c_bool(True)
-p5 = ctypes.c_bool (True)
-p6 = ctypes.c_char ("")
-p7 = ctypes.c_int (0)
-p8 = ctypes.c_int (0)
- 
-functionAPI (ctypes.byref(p1),ctypes.byref(p2),ctypes.byref(p3), p4,p5,p6,ctypes.byref(p7),ctypes.byref(p8),)
- 
- 
-"""
